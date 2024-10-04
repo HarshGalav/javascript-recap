96466b2a48adb400e192708754ff58121fc2f254
HarshGalav <harshsharma3122@gmail.com>
2024-10-04 22:17:19 +0530

    operators

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,12 +1,33 @@
 let score="33"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let temp = Number(score)
 
-console.log(typeof temp);
+// console.log(typeof temp);
 
-let islogged=1
+let islogged=1 
 let bislogged=Boolean(islogged);
-console.log(bislogged);
+// console.log(bislogged);
+
+let value=3
+let neg=-value
+// console.log(neg);
+
+let str1="hello"
+let str2=" Harsh"
+// console.log(str1+str2);
+
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(2+2+"1");
+
+console.log(+true);
+console.log(+"");
+
+
+
+
+
+
 
