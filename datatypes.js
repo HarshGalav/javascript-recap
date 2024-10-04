7db95d6c07cfe29ef7ea3715693542be4983f93c
HarshGalav <harshsharma3122@gmail.com>
2024-10-04 21:57:19 +0530

    datatypes done

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// alert("hello")
+
+let name="harsh"
+let age=20
+let isHuman=true
+// null->standalone value
+
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
