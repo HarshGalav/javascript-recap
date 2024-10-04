728e4be3d018a76e86a0354c18de20d027edc9a1
HarshGalav <harshsharma3122@gmail.com>
2024-10-04 21:47:04 +0530

    varibales done

---
+++ variable.js
@@ -0,0 +1,14 @@
+const id=1223
+let account="harsh"
+account="galav"
+var pass="anon"
+pass="jj"
+let temp;
+// id=3
+console.log(id);
+console.log(account);
+console.log(pass);
+console.table();
+console.log(temp);
+
+
